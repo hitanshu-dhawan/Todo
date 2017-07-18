034df9c7b6635c825a3c5524045a34592d544696
Hitanshu <hitanshudhawan1996@gmail.com>
2017-07-18 11:55:57 +0530

    fab and actionbar animation added

--- app/src/main/java/com/hitanshudhawan/todo/MainActivity.java
+++ app/src/main/java/com/hitanshudhawan/todo/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -13,11 +14,16 @@ import android.view.MenuItem;
 
 import java.util.ArrayList;
 
+import static com.hitanshudhawan.todo.R.id.fab;
+
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
     ArrayList<Todo> mTodos;
     TodoListAdapter mAdapter;
+
+    SearchView mSearchView;
+    FloatingActionButton mFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.todoRecyclerView);
+        mFab = (FloatingActionButton) findViewById(fab);
+
+        initView();
+
+    }
+
+    private void initView() {
 
         mTodos = new ArrayList<>();
         for(int i=0;i<20;i++) {
@@ -40,19 +53,44 @@ public class MainActivity extends AppCompatActivity {
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
 
+        mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if(newState == RecyclerView.SCROLL_STATE_IDLE)
+                    mFab.show();
+                else
+                    mFab.hide();
+            }
+        });
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             }
         });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchMenuItem = menu.findItem(R.id.search_item);
+        SearchView searchView = (SearchView) searchMenuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         return true;
     }
 
