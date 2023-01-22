57e21eb3719a70e5bcde7311ffb744705fc60947
Ankit Sharma <ankit.sharma722327@gmail.com>
2023-01-22 14:21:14 +0530

    Add Splash Screen in Android App.

---
+++ app/src/main/java/com/hitanshudhawan/todo/activities/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.hitanshudhawan.todo.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.hitanshudhawan.todo.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },5000);
+
+    }
+}
